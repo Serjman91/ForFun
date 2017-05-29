1c34414f85373e2137e9dd9293d39e1ca7aecb67
Serjman91 <serjman91@gmail.com>
2017-05-29 21:12:31 +0300

    fixed mistakes

--- Admin/api/api.php
+++ Admin/api/api.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     require_once 'utils.php';
     define("CATEGORY","Admin/DB/categories.json");
     define("INFO","Admin/DB/info.json");
@@ -11,12 +10,5 @@
     $info = getInfo();
     $banners = array();
 
-=======
-
-    $categories = array(1,2,3);
-    $items = array(6,6,5);
-    $info = "mail@mail.com";
-    $banners = array("ff", "ff");
->>>>>>> 9b1184254ac99917be48ea41bc1679792966a4d1
 ?>
 
--- Admin/views/content.php
+++ Admin/views/content.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php foreach ($items as $item){?>
 <div class="fun col-md-8">
  	<h2><?php echo $item[2];?></h2>
@@ -10,30 +9,3 @@
 
 <? require 'Admin/views/center_banner.php';?>
 
-=======
-<div class="fun col-md-8">
- 	<h2>title!</h2>
- 	<img class="img-rounded" src="img/fun/1.jpg">
-</div>
-
-<div class="fun col-md-8">
- 	<h2>title!</h2>
- 	<img class="img-rounded" src="img/fun/2.jpg">
-</div>
-
-<div class="fun col-md-8">
- 	<h2>title!</h2>
- 	<img class="img-rounded" src="img/fun/3.jpg">
-</div>
-
-<? require 'Admin/views/center_banner.php';?>
-
-<div class="fun col-md-8">
- 	<h2>title!</h2>
- 	<img class="img-rounded" src="img/fun/4.jpg">
-</div>
-<div class="fun col-md-8">
- 	<h2>title!</h2>
- 	<img class="img-rounded" src="img/fun/5.jpg">
-</div>
->>>>>>> 9b1184254ac99917be48ea41bc1679792966a4d1
